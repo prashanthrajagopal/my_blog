bb6751ca98f18d3f0c410026a7b3b0c893f722f3
Prashanth Rajagopal <prashanth.rajagopal@infibeam.net>
2014-01-15 17:18:32 +0530

    fonts assets precompiles properly now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Prashanthr::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.JPG)
+  config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.JPG *.eot)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
